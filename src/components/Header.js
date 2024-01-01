7be70af2a19b9fb4b7cd719fb93b127d4073e3e1
the-wolvorine <uday.cheekatipalli@slu.edu>
2024-01-01 14:56:52 -0600

    Added React router and linked nav links with their respective routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,20 @@ import React from "react";
 import Logo from "../Logo.svg";
 
 import "../styles/header.css";
+import {Link} from 'react-router-dom';
 
 export default function Header() {
     return(
         <header>
             <img src={Logo} alt="Little Lemon" className="img"/>
-            <nav>
-                <ul>
-                    <a href="/">Home</a>
-                    <a href="/">About</a>
-                    <a href="/">Menu</a>
-                    <a href="/">Reservations</a>
-                    <a href="/">Order Online</a>
-                    <a href="/">Login</a>
+            <nav className="header-nav">
+                <ul className="header-links">
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/menu'>Menu</Link></li>
+                    <li><Link to='/reservations'>Reservations</Link></li>
+                    <li><Link to='/orders'>Order Online</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
                 </ul>
             </nav>
         </header>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Reservations from './components/Reservations';
+import About from './components/About';
+import Login from './components/Login';
+import Order from './components/Order';
+import Menu from './components/Menu';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const Routing = () => {
+  return(
+    <Router>
+      <Header/>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path='/reservations' element={<Reservations />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Order />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  )
+}
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
