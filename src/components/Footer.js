940caa50db717957fbb1207192d2d07818ed80d7
the-wolvorine <uday.cheekatipalli@slu.edu>
2023-12-31 18:59:41 -0600

    Added header, main and footer components. Updated Meta tags.

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+export default function Footer() {
+    return(
+        <footer>
+            <nav>
+                <p>Doormat Navigation</p>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Menu</a></li>
+                    <li><a href="/">Reservations</a></li>
+                    <li><a href="/">Order Online</a></li>
+                    <li><a href="/">Login</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <p>Contact Us</p>
+                <ul>
+                    <li><a href="/">Address</a></li>
+                    <li><a href="/">Phone Number</a></li>
+                    <li><a href="/">email</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <p>Social Media Links</p>
+                <ul>
+                    <li><a href="/">Facebook</a></li>
+                    <li><a href="/">Instagram</a></li>
+                    <li><a href="/">Pintrest</a></li>
+                </ul>
+            </nav>
+        </footer>
+    );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../Logo.svg";
+
+export default function Header() {
+    return(
+        <header>
+            <img src={Logo} alt="Little Lemon" />
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Menu</a></li>
+                    <li><a href="/">Reservations</a></li>
+                    <li><a href="/">Order Online</a></li>
+                    <li><a href="/">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
